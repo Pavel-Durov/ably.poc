7cb9cecc325ef6b393ff2e085cc76f2778471220
Pavel Durov <pavel@p3ld3v.dev>
2023-11-13 16:31:07 +0000

    chore: channel presence.

--- src/channels.ts
+++ src/channels.ts
@@ -6,20 +6,31 @@ const log = console.log;
 const channelName = 'pavel-poc';
 
 async function sub() {
+  const client = 'subber';
   const ably = new Ably.Realtime.Promise(API_KEY);
   await ably.connection.once('connected');
 
   log(`Subscribed to channel: ${channelName}`);
   const channel = ably.channels.get(channelName);
+  channel.presence.enterClient(client);
+
+  channel.presence.subscribe('enter', function (member) {
+    if (member.clientId === client) {
+      console.log(`Got new joiner: ${member.clientId}`);
+    }
+  });
+
   await channel.subscribe(channelName, (message) => {
     log(`Got message: ${message.data}`);
   });
 }
 
 async function pub() {
+  const client = 'pubber';
   const ably = new Ably.Realtime.Promise(API_KEY);
   await ably.connection.once('connected');
   const channel = ably.channels.get(channelName);
+  channel.presence.enterClient(client); // => Bob entered realtime-chat
   log(`Published to channel: ${channelName}`);
   await channel.publish(channelName, 'hello!');
 }
